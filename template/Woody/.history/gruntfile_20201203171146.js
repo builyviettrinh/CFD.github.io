69a19e54a19dbe30c71667ea48cf81b759d85066
builyviettrinh <builyviettrinh@gmail.com>
2020-12-06 01:47:50 +0700

    update 5/12

---
+++ template/Woody/.history/gruntfile_20201203171146.js
@@ -0,0 +1,22 @@
+// Gruntfile.js
+module.exports = grunt => {
+    // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+    require('load-grunt-tasks')(grunt);
+ 
+    grunt.initConfig({
+        //config
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+			options:{
+				keepSpecialComments: 0
+			},
+			my_target: {
+				files: [{
+					'<%= dirs.dest %>/stylelibs.min.css' : ['<%= dirs.css %>/**/*.css']
+				}]
+			}
+		},
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', []);
+};
