699286178e42d3db83cc1c064c63f3ac0c133363
builyviettrinh <builyviettrinh@gmail.com>
2020-12-07 00:24:38 +0700

    update

--- template/GruntTemplate/.history/gruntfile_20201203165525.js
+++
@@ -1,8 +0,0 @@
-// Gruntfile.js
-module.exports = grunt => {
-    // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
-    require('load-grunt-tasks')(grunt);
- 
-    grunt.initConfig({});
-    grunt.registerTask('default', []);
-};
