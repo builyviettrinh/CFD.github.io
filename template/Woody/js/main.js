3ff8c2c8976dfe9ffa19b6d13e8b2e7eab63c5ee
builyviettrinh <builyviettrinh@gmail.com>
2020-12-14 01:33:53 +0700

    update

---
+++ template/Woody/js/main.js
@@ -0,0 +1,27 @@
+// backtotop
+//create smooth scroll on click
+ 
+document.getElementById("backToTop").addEventListener('click', function(e) {
+  e.preventDefault();  
+ window.scrollTo({top: 0, behavior: "smooth"});
+});
+
+
+//hide/show button on scroll up/down
+let scrollPos = 0;
+
+window.addEventListener('scroll', function(){
+
+  // detects new state and compares it with the new one
+  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    console.log('scrolling down')
+    
+    document.getElementById('backToTop').classList.remove('active');
+    
+  } else {
+		document.getElementById('backToTop').classList.add('active');
+    console.log('scrolling up')	
+  }
+  // saves the new position for iteration.
+  scrollPos = (document.body.getBoundingClientRect()).top;
+});
